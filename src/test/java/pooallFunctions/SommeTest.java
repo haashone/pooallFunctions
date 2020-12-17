d98eec394216113c314db448694e6c29d08f2d35
haashone <hachone10@gmail.com>
2020-12-17 06:40:34 +0100

    adding tests

--- src/test/java/pooallFunctions/SommeTest.java
+++ src/test/java/pooallFunctions/SommeTest.java
@@ -3,6 +3,8 @@ package pooallFunctions;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import models.Calcul;
 
 public class SommeTest {
@@ -11,8 +13,17 @@ public class SommeTest {
 	public void testSomme() {
 		
 		Calcul c = new Calcul();
-		
+		assertNotNull(c);
 		assertEquals("", 10.0, c.somme(5, 5), 0.0);
 	}
+	
+	@Test
+	public void testMultiplication() {
+		
+		Calcul c = new Calcul();
+		assertNotNull(c);
+		assertEquals("", 25.0, c.multi(5, 5), 0.0);
+	}
+
 
 }
